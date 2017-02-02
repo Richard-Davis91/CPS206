fbca76e4ae85a51b07b915ec3744060baae890b2
Richard-Davis91 <mogiz91@gmail.com>
2017-02-02 08:27:17 -0600

    recursion

--- RecursionExample.java
+++ RecursionExample.java
@@ -1,6 +1,14 @@
 public class RecursionExample{
-
+int r = 0;
   public static void main(String[] args){
-  //do factorial
+
+  RecursionExample recursion = new RecursionExample();
+  System.out.println(recursion.doFactorial(7));
+  }
+
+  public int doFactorial (int n){
+    //do Factorial -> Goal - no for/while/do-while loops
+    if (n == 1) return 1;
+    return n * doFactorial(n-1);
   }
 }
